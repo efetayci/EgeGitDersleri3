6329b162e12c6e4c51c9a60b41e0ac847882e348
Efe Taycı <efefehmitayci@gmail.com>
2022-12-25 13:46:01 +0300

    second commit

--- EgeGit2/Program.cs
+++ EgeGit2/Program.cs
@@ -7,6 +7,7 @@ namespace EgeGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Ege github ve git kullanıyor");
+            Console.WriteLine("Second Commit");
         }
     }
 }

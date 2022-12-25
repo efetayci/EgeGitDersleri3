92d44f24d9348271cdfe5d79f312da17deefb8d2
Efe Taycı <efefehmitayci@gmail.com>
2022-12-25 13:39:46 +0300

    ilk komitim

---
+++ EgeGit2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EgeGit2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ege github ve git kullanıyor");
+        }
+    }
+}
